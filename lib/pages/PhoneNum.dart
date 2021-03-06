437128fc7f5f051934b6bb8765b1d013aa04d0de
Doyeon Baek <drakedog19@gmail.com>
2021-03-06 20:01:14 +0900

    Fixed

--- lib/pages/PhoneNum.dart
+++ lib/pages/PhoneNum.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:country_picker/country_picker.dart';
+import 'package:http/http.dart' as http;
 
 class PhoneNum extends StatefulWidget {
   static const id = 'phone_num';
@@ -11,6 +14,11 @@ class PhoneNum extends StatefulWidget {
 class _PhoneNumState extends State<PhoneNum> {
   String countryCode = 'kr';
   int phoneCode = 82;
+  String phoneNum;
+  String verificationCode;
+  String verificationId;
+  int resendToken;
+  bool isVerificationSMSSent = false;
 
   @override
   Widget build(BuildContext context) {
@@ -31,84 +39,157 @@ class _PhoneNumState extends State<PhoneNum> {
       ),
       body: Align(
         alignment: Alignment.center,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+        child: Stack(
           children: [
-            SizedBox(
-              height: 30.0,
-            ),
-            Text(
-              "Enter your phone #",
-              style: TextStyle(
-                fontSize: 20.0,
-              ),
-            ),
-            SizedBox(
-              height: 50.0,
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width - 80,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(5.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 10.0,
-                ),
-                child: Row(
+            Offstage(
+              offstage: isVerificationSMSSent,
+              child: TickerMode(
+                enabled: !isVerificationSMSSent,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Expanded(
-                      flex: 3,
-                      child: MaterialButton(
-                        padding: EdgeInsets.zero,
-                        child: Row(
-                          children: [
-                            SizedBox(
-                              width: 25.0,
-                              child: Image.asset(
-                                'icons/flags/png/$countryCode.png',
-                                package: 'country_icons',
-                              ),
-                            ),
-                            SizedBox(
-                              width: 10.0,
-                            ),
-                            Text('+$phoneCode',
-                                style: TextStyle(
-                                  fontSize: 18.0,
-                                  letterSpacing: -1.0,
-                                )),
-                          ],
-                        ),
-                        onPressed: () {
-                          showCountryPicker(
-                            context: context,
-                            showPhoneCode: true,
-                            // optional. Shows phone code before the country name.
-                            onSelect: (Country country) {
-                              setState(() {
-                                countryCode = country.countryCode.toLowerCase();
-                                phoneCode = int.parse(country.phoneCode);
-                              });
-                            },
-                          );
-                        },
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                    Text(
+                      "Enter your phone #",
+                      style: TextStyle(
+                        fontSize: 20.0,
                       ),
                     ),
-                    Expanded(
-                      flex: 8,
-                      child: TextField(
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          focusedBorder: InputBorder.none,
-                          enabledBorder: InputBorder.none,
-                          errorBorder: InputBorder.none,
-                          disabledBorder: InputBorder.none,
-                          contentPadding: EdgeInsets.only(
-                              left: 15, bottom: 11, top: 11, right: 15),
-                          hintText: "Phone Number",
+                    SizedBox(
+                      height: 50.0,
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width - 80,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 10.0,
+                        ),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              flex: 3,
+                              child: MaterialButton(
+                                padding: EdgeInsets.zero,
+                                child: Row(
+                                  children: [
+                                    SizedBox(
+                                      width: 25.0,
+                                      child: Image.asset(
+                                        'icons/flags/png/$countryCode.png',
+                                        package: 'country_icons',
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: 10.0,
+                                    ),
+                                    Text(
+                                      '+$phoneCode',
+                                      style: TextStyle(
+                                        fontSize: 18.0,
+                                        letterSpacing: -1.0,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                onPressed: () {
+                                  showCountryPicker(
+                                    context: context,
+                                    showPhoneCode: true,
+                                    // optional. Shows phone code before the country name.
+                                    onSelect: (Country country) {
+                                      setState(() {
+                                        countryCode =
+                                            country.countryCode.toLowerCase();
+                                        phoneCode =
+                                            int.parse(country.phoneCode);
+                                      });
+                                    },
+                                  );
+                                },
+                              ),
+                            ),
+                            Expanded(
+                              flex: 8,
+                              child: TextField(
+                                keyboardType: TextInputType.number,
+                                onChanged: (_phoneNum) {
+                                  phoneNum = _phoneNum;
+                                },
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  focusedBorder: InputBorder.none,
+                                  enabledBorder: InputBorder.none,
+                                  errorBorder: InputBorder.none,
+                                  disabledBorder: InputBorder.none,
+                                  contentPadding: EdgeInsets.only(
+                                      left: 15, bottom: 11, top: 11, right: 15),
+                                  hintText: "Phone Number",
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 70.0,
+                      ),
+                      child: Text(
+                        "By entering your number, you're agreeing to our Terms of Service and Privacy Policy. Thanks!",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          height: 1.8,
+                          fontSize: 10.0,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Material(
+                        color: Colors.grey[400],
+                        borderRadius: BorderRadius.circular(
+                          30.0,
+                        ),
+                        child: MaterialButton(
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 50.0,
+                            ),
+                            child: Text(
+                              "Next ->",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18.0,
+                              ),
+                            ),
+                          ),
+                          onPressed: () async {
+                            await http.post(
+                                Uri.http('korjarvis.hopto.org:8091', '/send'),
+                                body: {
+                                  'phoneNumber': phoneNum,
+                                  'countryCode': phoneCode.toString(),
+                                }).then((res) {
+                              if (jsonDecode(res.body)["status"] == "success") {
+                                setState(() {
+                                  isVerificationSMSSent = true;
+                                });
+                              }
+                            });
+                          },
                         ),
                       ),
                     ),
@@ -116,51 +197,146 @@ class _PhoneNumState extends State<PhoneNum> {
                 ),
               ),
             ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: 70.0,
-              ),
-              child: Text(
-                "By entering your number, you're agreeing to our Terms of Service and Privacy Policy. Thanks!",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  height: 1.8,
-                  fontSize: 10.0,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Align(
-              alignment: Alignment.center,
-              child: Material(
-                color: Colors.grey[400],
-                borderRadius: BorderRadius.circular(
-                  30.0,
-                ),
-                child: MaterialButton(
-                  minWidth: 0,
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 50.0,
+            Offstage(
+              offstage: !isVerificationSMSSent,
+              child: TickerMode(
+                enabled: isVerificationSMSSent,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 50.0,
                     ),
-                    child: Text(
-                      "Next ->",
+                    Text(
+                      "Enter the code we\njust texted you",
+                      textAlign: TextAlign.center,
                       style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18.0,
+                        fontSize: 20.0,
+                        height: 1.8,
                       ),
                     ),
-                  ),
-                  onPressed: () {
-                  },
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width - 80,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 10.0,
+                        ),
+                        child: TextField(
+                          textAlign: TextAlign.center,
+                          keyboardType: TextInputType.number,
+                          onChanged: (_verificationCode) {
+                            verificationCode = _verificationCode;
+                          },
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                            enabledBorder: InputBorder.none,
+                            errorBorder: InputBorder.none,
+                            disabledBorder: InputBorder.none,
+                            contentPadding: EdgeInsets.only(
+                                left: 15, bottom: 11, top: 11, right: 15),
+                            hintText: "⦁ ⦁ ⦁ ⦁ ⦁ ⦁",
+                            hintStyle: TextStyle(
+                              fontSize: 18.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      "Didn't receive it? Tap to resend",
+                      style: TextStyle(
+                        color: Colors.black54,
+                        height: 1.8,
+                        fontSize: 13.0,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 50.0,
+                    ),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Material(
+                        color: Colors.grey[400],
+                        borderRadius: BorderRadius.circular(
+                          30.0,
+                        ),
+                        child: MaterialButton(
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 50.0,
+                            ),
+                            child: Text(
+                              "Next ->",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18.0,
+                              ),
+                            ),
+                          ),
+                          onPressed: () async {
+                            await http.post(
+                                Uri.http(
+                                    'korjarvis.hopto.org:8091', '/confirm'),
+                                body: {
+                                  "phoneNumber": phoneNum,
+                                  "verifyCode": verificationCode.toString(),
+                                }).then((res) async {
+                              if (jsonDecode(res.body)["status"] == "success") {
+                                await showDialog(
+                                  context: context,
+                                  builder: (context) => AlertDialog(
+                                    title: Text('Congrats!'),
+                                    content: Text('Authentication succeed.'),
+                                    actions: [
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.of(context,
+                                                  rootNavigator: true)
+                                              .pop(); // dismisses only the dialog and returns nothing
+                                        },
+                                        child: Text('OK'),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              } else {
+                                await showDialog(
+                                  context: context,
+                                  builder: (context) => AlertDialog(
+                                    title: Text('Too bad!'),
+                                    content: Text('Authentication failed.'),
+                                    actions: [
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.of(context,
+                                              rootNavigator: true)
+                                              .pop(); // dismisses only the dialog and returns nothing
+                                        },
+                                        child: Text('OK'),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              }
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
+            )
           ],
         ),
       ),
